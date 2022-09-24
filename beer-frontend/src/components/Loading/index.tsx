29114d07083a8fa137b27c0b53dc806b9b35003c
Sikkapat Sricheangsa <bank.s.sricheangsa@gmail.com>
2022-09-24 15:04:10 +0700

    feat(loading): turn no-beer into loading component

---
+++ beer-frontend/src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import './Loading.css';
+
+const Loading = () => {
+  return (
+    <div className='loading'>
+      <img className='loading__icon' src='/logo192.png' alt='random-beer' />
+    </div>
+  );
+};
+export default Loading;
