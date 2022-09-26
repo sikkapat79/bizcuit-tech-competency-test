0a928472caea878e25998fad5875c057154e3e0d
Sikkapat Sricheangsa <bank.s.sricheangsa@gmail.com>
2022-09-26 10:12:23 +0700

    feat: add missing interface for configuration

---
+++ beer-api/src/configuration/index.ts
@@ -0,0 +1,11 @@
+export default interface IConfiguariton {
+  // Global environment
+  NOD_ENV: string;
+
+  // Database Configuraion
+  DB_USER: string;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_PASSWORD: string;
+  DB_DATABASE: string;
+}
