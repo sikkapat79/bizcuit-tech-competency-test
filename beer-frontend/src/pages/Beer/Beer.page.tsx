fd94ac699175ed97836c63fa1e12aa00c16ae44d
Sikkapat Sricheangsa <bank.s.sricheangsa@gmail.com>
2022-09-26 10:12:54 +0700

    refactor: remove timeout while fetching data

--- beer-frontend/src/pages/Beer/Beer.page.tsx
+++ beer-frontend/src/pages/Beer/Beer.page.tsx
@@ -33,10 +33,8 @@ const BeerPage = () => {
     setIsLoading(() => true);
     const _beer = await getRandomBeer();
 
-    setTimeout(() => {
-      setIsLoading(() => false);
-      addBeer({ ..._beer });
-    }, 2000);
+    setIsLoading(() => false);
+    addBeer({ ..._beer });
   };
 
   const handlePreviousBeerClick = selectPreviousBeer;
