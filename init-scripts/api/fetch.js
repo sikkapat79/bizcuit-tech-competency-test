46c0d030e0996fbfb919d8dd2e1080a9fe078e6f
Sikkapat Sricheangsa <bank.s.sricheangsa@gmail.com>
2022-09-26 10:15:12 +0700

    build: add script to mock beer data

---
+++ init-scripts/api/fetch.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const RANDOM_BEER_URI = 'https://random-data-api.com/api/beer/random_beer';
+const ADD_BEER_URI = 'http://host.docker.internal:4200/v1/beers';
+
+const fetchRandomBeer = async () => {
+  try {
+    const response = await fetch(RANDOM_BEER_URI);
+    const { id, ...beer } = await response.json();
+
+    return beer;
+  } catch (err) {
+    console.error(`[ERROR] can't fetch beer from ${RANDOM_BEER_URI}`);
+    console.error(err);
+
+    throw err;
+  }
+};
+
+const addBeer = async (beer) => {
+  try {
+    const response = await fetch(ADD_BEER_URI, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(beer),
+    });
+
+    return await response.json();
+  } catch (err) {
+    console.error(`[ERROR] can't fetch beer from ${ADD_BEER_URI}`);
+    console.error(err);
+
+    throw err;
+  }
+};
+
+const initDb = async () => {
+  console.log('[INFO] add new beer into database for 20 beers.');
+  let beerStore = new Array(20).fill(null);
+
+  beerStore.map(async () => {
+    const fetchedBeer = await fetchRandomBeer();
+    const beer = await addBeer(fetchedBeer);
+
+    return beer;
+  });
+
+  beerStore = await Promise.all(beerStore);
+  console.table(beerStore);
+};
+
+initDb();
